011009a30e85514fe2c1c421c779bf00653a9801
Akshit <akshit.t@aftershoot.com>
2026-01-17 00:22:08 +0530

    example of tightly coupled code - don't do this

---
+++ bad_example/tightly_coupled.go
@@ -0,0 +1,38 @@
+package bad_example
+
+import (
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type URLService struct{}
+
+func NewURLService() *URLService {
+	return &URLService{}
+}
+
+func (s *URLService) SaveURL(shortCode, longURL string) error {
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	defer client.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return client.Set(ctx, shortCode, longURL, 24*time.Hour).Err()
+}
+
+func (s *URLService) GetURL(shortCode string) (string, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	defer client.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return client.Get(ctx, shortCode).Result()
+}
